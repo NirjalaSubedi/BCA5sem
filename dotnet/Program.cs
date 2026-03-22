b31f3f31c62da1cf6cd12308a00c55a9a40701b0
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-22 20:33:57 +0545

    first commit

---
+++ dotnet/Program.cs
@@ -0,0 +1,10 @@
+﻿using Program; // Mathi namespace import garne
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Typeconversion class ko method lai call gareko
+        Typeconversion.MyLogic(); 
+    }
+}
---
+++ dotnet/TypeConversion.cs
@@ -0,0 +1,10 @@
+namespace Typeconversion;
+
+class Typeconversion
+{
+    // static rakhema direct class ko naam le call garna milchha
+    public static void MyLogic() 
+    {
+        Console.WriteLine("hello world\n Iam learning c#");
+    }
+}
