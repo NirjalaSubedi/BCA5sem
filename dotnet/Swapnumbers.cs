75b90fa778df3f50a3b367f87cb2df881ab68a8c
Nirjala Subedi <nirjalasubedi944@gmail.com>
2026-03-22 20:55:54 +0545

    Implement SwapTwoNumbers method with console output in Swap class

--- dotnet/Swapnumbers.cs
+++ dotnet/Swapnumbers.cs
@@ -3,7 +3,14 @@ namespace Swapnumbers
     class Swap{
         public static void SwapTwoNumbers()
         {
-            
+            Console.WriteLine("lets swap two numbers using another variable\n");
+            int num1=6,num2=9,num3;
+            Console.WriteLine("num1\t"+num1+"\tnum2\t"+num2+"\n");
+            num3=num2;
+            num2=num1;
+            Console.WriteLine("num1\t"+num3+"\tnum2\t"+num2);
+
+
         }
     }
 }
